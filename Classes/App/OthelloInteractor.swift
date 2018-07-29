12ea4eb846ab96ef244b7f77b5f1e7c3547dcbd5
Petteri Kamppuri <petteri.kamppuri@me.com>
2018-07-29 13:39:52 +0300

    Added class which will be the destination of extracting logic from ViewController.
    This is to separate UI from state and logic, and make state and logic separately testable.

---
+++ Classes/App/OthelloInteractor.swift
@@ -0,0 +1,13 @@
+//
+//  OthelloInteractor.swift
+//  Othello
+//
+//  Created by Petteri Kamppuri on 29.7.2018.
+//  Copyright © 2018 Petteri Kamppuri. All rights reserved.
+//
+
+import Foundation
+
+class OthelloInteractor {
+    
+}
